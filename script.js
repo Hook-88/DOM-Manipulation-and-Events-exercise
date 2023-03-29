531513d2cfd193db7bd27306b8b5690f4532ca8b
Paul Hoekjan <paulhoekjan@gmail.com>
2023-03-29 11:11:05 -0500

    Add logic for creating, adding elements with css inline styles

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const container = document.querySelector('#container');
 const redParagraph = document.createElement('p');
+const blueHeader3 = document.createElement('h3');
+const elementsContainer = document.createElement('div');
+const header1 = document.createElement('h1');
+const paragraph = document.createElement('p');
+
+
 redParagraph.textContent = "Hey I’m red!";
 redParagraph.style.color = 'red';
+
+blueHeader3.textContent = "I'm a blue h3!";
+blueHeader3.style.color = 'blue';
+
+elementsContainer.style.backgroundColor = 'pink';
+elementsContainer.style.border ='thin solid black';
+
+header1.textContent = "I'm a div";
+paragraph.textContent = "ME TOO!";
+
 container.appendChild(redParagraph);
+container.appendChild(blueHeader3);
+elementsContainer.appendChild(header1);
+elementsContainer.appendChild(paragraph);
+container.appendChild(elementsContainer);
+
